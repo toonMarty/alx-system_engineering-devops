cf398dae18bd1305333dcaffdfb21e03b7cabfe3
toonMarty <martinnyaga7373@gmail.com>
2022-10-14 01:56:48 -0700

    Create a file

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,1 +1,1 @@
-file {'/tmp/schoool': ensure => 'present', mode => '0744', owner => 'www-data', group => 'www-data', content => 'I love Puppet'}
+file {'/tmp/school': ensure => 'present', mode => '0744', owner => 'www-data', group => 'www-data', content => 'I love Puppet'}
