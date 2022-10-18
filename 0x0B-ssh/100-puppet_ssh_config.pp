468053dd2bcba7f4a05ca306d02492f6c8d53588
toonMarty <martinnyaga7373@gmail.com>
2022-10-18 16:28:22 -0700

    task 100

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++
@@ -1,9 +0,0 @@
-file_line {'Identity_file':
-  ensure => 'present',
-  path   => '/etc/ssh/ssh_config',
-  line   => 'Identity_file ~/.ssh/school',
-}
-file_line {'disable password login':
-  path => '/etc/ssh/ssh_config',
-  line => ' PasswordAuthentication no'
-}
