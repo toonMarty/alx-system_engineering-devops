9378b9697b95aceb8913ef65efa886a46b400cb0
toonMarty <martinnyaga7373@gmail.com>
2022-12-10 06:49:44 +0000

    Sky is the limit, lets bring that limit higher

---
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -0,0 +1,11 @@
+# Increase traffic in nginx server
+exec {'fix--for-nginx':
+  command => 'sed -i "s/15/4096/" /etc/default/nginx',
+  path    => '/usr/local/bin/:/bin/'
+} ->
+
+# restart nginx
+exec { 'nginx-restart':
+  command => 'nginx restart',
+  path    => '/etc/init.d/'
+}
